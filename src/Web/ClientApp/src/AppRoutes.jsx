92714d8bf8015ec84edb0ae6bf19f1973655e349
TinTranT <trantrungtin254@gmail.com>
2025-10-04 21:27:01 +0700

    Change the folder name of Cart Page

--- src/Web/ClientApp/src/AppRoutes.jsx
+++ src/Web/ClientApp/src/AppRoutes.jsx
@@ -3,7 +3,7 @@ import Counter from "./components/Counter";
 import ThemeDemo from "./components/ThemeDemo";
 import UserLayout from "./layouts/UserLayout";
 import Homepage from "./features/guest/homepage/Homepage";
-import CartPage from "./features/user/CartPage/CartPage";
+import CartPage from "./features/user/cart-page/CartPage";
 import Register from "./features/guest/auth/register/Register";
 import Login from "./features/guest/auth/login/Login";
 import ForgetPassword from "./features/guest/auth/forgetpassword/ForgetPassword";

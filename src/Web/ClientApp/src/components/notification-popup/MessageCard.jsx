76ab5fe2e068ec759a13a5255c7750ad6940c529
Nguyen Nhat An <122602156+Nhatan1205@users.noreply.github.com>
2026-03-14 22:46:10 +0700

    fix empty title in section and item (#56)
    * fix empty title in section and item
    
    * fix rec

--- src/Web/ClientApp/src/components/notification-popup/MessageCard.jsx
+++ src/Web/ClientApp/src/components/notification-popup/MessageCard.jsx
@@ -1,7 +1,7 @@
 import { Avatar, Box, MenuItem, Typography } from "@mui/material";
 import DefaultImage from "../../assets/images/default.jpg";
 import { formatTimeAgo } from "../../utils/helpers";
-import useUpdateNotificationStatus from "../../hooks/useUpdateNoificationStatus";
+import useUpdateNotificationStatus from "../../hooks/useUpdateNotificationStatus";
 function MessageCard({ notification }) {
   const { id, title, created, isRead, imageUrl } = notification;
   const updateNotificationStatusMutation = useUpdateNotificationStatus();
